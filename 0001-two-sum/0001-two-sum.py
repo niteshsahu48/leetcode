074f8d2d5b122582f32fe9a345d3259987ea4f69
Nitesh Sahu <nitesh08815@gmail.com>
2025-01-07 18:42:45 +0530

    Time: 2046 ms (8.19%), Space: 18.6 MB (25.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         total = [] 
         for i in range (len(nums)): 
 	        for j in range (i+1,len(nums)):
