93361fc60896eae78410995a439d609d4a81baff
Nitesh Sahu <nitesh08815@gmail.com>
2025-03-10 18:03:08 +0530

    Time: 39 ms (63.78%), Space: 17.9 MB (24.39%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        """if n <= 0:
+        # use swaping
+        if n <= 0:
             return 0
         elif n == 1:
             return 1
@@ -13,11 +14,12 @@ class Solution:
             c = b
             b = a + b  
 
-        return b"""
+        return b
 
-        if n <= 1:
+        #by class 
+        """if n <= 1:
             return n
-        return self.fib(n-1)+self.fib(n-2)
+        return self.fib(n-1)+self.fib(n-2)"""
 
         
         
